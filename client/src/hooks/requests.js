18c5aa9b299571c7dffab66023802621d0a0cc54
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2023-06-26 08:51:47 +0530

    SERVER ISSUE

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,4 +1,4 @@
-const API_URL = 'v1';
+const API_URL = 'http://localhost:8000';
 
 // Load planets and return as JSON.
 async function httpGetPlanets() {
--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,16 @@
-const http=require('http');
-const PORT=process.env.PORT ||8000;
-const app=require('./app')
-console.log(PORT)
-const server=http.createServer(app);
-server.listen=(PORT,()=>{
-    console.log(`Listening on PORT ${PORT}.....`)
-});
+const http = require('http');
+
+
+const app = require('./app');
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+
+  
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
+
+
