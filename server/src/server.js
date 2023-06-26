fbf7b3a8a9b7bf58be297ddd76fe1130fd3e4a58
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2023-06-26 08:33:32 +0530

    SERVER ISSUE

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,8 @@
 const http=require('http');
-const PORT=8000;
+const PORT=process.env.PORT ||8000;
 const app=require('./app')
+console.log(PORT)
 const server=http.createServer(app);
-server.listen=PORT,()=>{
+server.listen=(PORT,()=>{
     console.log(`Listening on PORT ${PORT}.....`)
-}
+});
