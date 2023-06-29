6e101fc9e485ab51119fa84ce808090bbdaa1bae
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2023-06-29 11:58:47 +0530

    endpoint

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -2,7 +2,7 @@ const API_URL = 'http://localhost:8000';
 
 // Load planets and return as JSON.
 async function httpGetPlanets() {
-  const response = await fetch(`${API_URL}/planets`);
+  const response = await fetch(`${API_URL}/trains`);
   return await response.json();
 }
 
--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 const express=require('express');
+const cors=require('cors');
 const trainsrouter=require('./routes/Trains/Trains.router')
 const app=express();
 
 
 //middleware
+app.use(cors({
+    origin:"http://localhost:3000", //can use callback whenerver necessary mantain whitelist array
+}));
 app.use(express.json());
 app.use(trainsrouter);
 module.exports=app;
